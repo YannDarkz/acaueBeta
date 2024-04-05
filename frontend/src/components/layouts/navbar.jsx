a6213c8f827985ec1362b03badaed2904c870a6a
yanndarkz <iansantosmcqueen@gmail.com>
2024-04-04 23:56:53 -0300

    att

--- frontend/src/components/layouts/navbar.jsx
+++ frontend/src/components/layouts/navbar.jsx
@@ -2,15 +2,41 @@ import styles from './navbar.module.scss';
 import SocialMedia from './socialMedia';
 import { Link } from 'react-router-dom';
 
+import { useState, useRef, useEffect } from 'react';
+
 
 const NavBar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setMenuOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    console.log("referencia", menuRef)
+
 
     return (
         <nav className={styles.container_nav}>
 
-            <input type="checkbox" id="menu_hamburguer" />
+            <input type="checkbox" id="menu_hamburguer" checked={menuOpen} onChange={toggleMenu} />
 
-            <label htmlFor="menu_hamburguer">
+            <label htmlFor="menu_hamburguer" ref={menuRef}>
                 <div className={styles.menu}>
                     <span className={styles.hamburguer}></span>
                 </div>
